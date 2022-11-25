3e0d4527e013162623d1465a730efe47ea980201
WooLyung <vlfkdydtk@naver.com>
2022-11-25 16:04:11 +0900

    서버 연결

--- app/src/main/java/milk/example/platform/Client.java
+++ app/src/main/java/milk/example/platform/Client.java
@@ -21,7 +21,7 @@ public final class Client {
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         OkHttpClient logger = new OkHttpClient.Builder().addInterceptor(interceptor).readTimeout(20L, TimeUnit.SECONDS).writeTimeout(20L, TimeUnit.SECONDS).build();
-        Retrofit retrofit = new retrofit2.Retrofit.Builder().baseUrl("http://10.0.2.2:8080/").addConverterFactory(GsonConverterFactory.create(gson)).client(logger).build();
+        Retrofit retrofit = new retrofit2.Retrofit.Builder().baseUrl("http://34.64.220.6:8080/").addConverterFactory(GsonConverterFactory.create(gson)).client(logger).build();
         retrofitService = retrofit.create(RetrofitAPI.class);
     }
 }
