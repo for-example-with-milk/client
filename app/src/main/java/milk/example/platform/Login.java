0718845cca68225fe9d9c6b982d8519aad14eca2
minju26 <mjalswn26@gmail.com>
2022-11-28 10:29:31 +0900

    login_0

---
+++ app/src/main/java/milk/example/platform/Login.java
@@ -0,0 +1,16 @@
+package milk.example.platform;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+    }
+}
