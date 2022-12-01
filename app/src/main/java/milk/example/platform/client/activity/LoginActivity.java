7c0df87503c05bdbf09847c425d265e2e1b3aa14
kwonssshyeon <kwonsh4396@gmail.com>
2022-12-01 21:19:54 +0900

    commit for merge

--- app/src/main/java/milk/example/platform/client/activity/LoginActivity.java
+++ app/src/main/java/milk/example/platform/client/activity/LoginActivity.java
@@ -4,19 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import milk.example.platform.client.R;
 import milk.example.platform.client.conductor.AccountConductor;
 
 public class LoginActivity extends AppCompatActivity {
     private AccountConductor conductor;
-
     private Button login;
+    private EditText id;
+    private String pw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.login);
+
+        id = (EditText)findViewById(R.id.id);
 
         conductor = new AccountConductor(getApplicationContext(), this);
 
--- app/src/main/java/milk/example/platform/client/packet/responseBody/LoginResponseBody.java
+++ app/src/main/java/milk/example/platform/client/packet/responseBody/LoginResponseBody.java
@@ -14,4 +14,5 @@ public class LoginResponseBody {
     public boolean getUser() {
         return user;
     }
+
 }
